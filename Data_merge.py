d220566f9bbfd33ef02938b651fd4c940b967081
Latexinz <61430003+Latexinz@users.noreply.github.com>
2023-10-04 13:45:45 +0300

    Add files via upload
    Splits the data into training, validation and testing data 60/20/20

--- Data_merge.py
+++ Data_merge.py
@@ -6,12 +6,13 @@ Merges pose detection keypoint data with EEG data by timestamp.
 
 import glob
 import pandas as pd
+import numpy as np
 
 from Filters import butter_highpass_filter, butter_bandpass_filter, Implement_Notch_Filter
 
 
 
-EEG_data = pd.concat([pd.read_table(file, sep='\t') for file in glob.glob('sessions/EEG*.csv')])        
+EEG_data = pd.concat([pd.read_table(file, sep='\t') for file in glob.glob('sessions/EEG*.csv')])        #all sessions into a single dataframe
 EEG_data = EEG_data.drop(EEG_data.columns[[0, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31]], axis=1)
 EEG_data['Timestamp'] = EEG_data['Timestamp'].round(2)
 EEG_data = EEG_data[:-1250]
@@ -31,7 +32,9 @@ Pose_data = pd.concat([pd.read_csv(file, delimiter=',') for file in glob.glob('s
 Pose_data['Timestamp'] = Pose_data['Timestamp'].round(2)
 Pose_data = Pose_data[:-600]
 
-merged_data = Pose_data.merge(EEG_data, how='inner', on='Timestamp')
+merged_data = Pose_data.merge(EEG_data, how='inner', on='Timestamp') #merge the datasets
 merged_data = merged_data.drop(['Timestamp'], axis=1)
-merged_data.to_csv('prepared_data/Data.csv', sep=',', index=False)
-
+trn_data, vld_data, tst_data = np.split(merged_data, [int(.6*len(merged_data)), int(.8*len(merged_data))]) #split the data into training, validation and testing data
+trn_data.to_csv('prepared_data/Training_Data.csv', sep=',', index=False)
+vld_data.to_csv('prepared_data/Validation_Data.csv', sep=',', index=False)
+tst_data.to_csv('prepared_data/Test_Data.csv', sep=',', index=False)
