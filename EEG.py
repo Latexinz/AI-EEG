e8b01d12f559ceaafd755c101418f7938bbe00d1
Latexinz <61430003+Latexinz@users.noreply.github.com>
2023-10-10 13:57:37 +0300

    Add files via upload
    Cleaned it up a bit

--- EEG.py
+++ EEG.py
@@ -17,24 +17,14 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter
 
 
-parser = argparse.ArgumentParser() # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
-parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False, default=0)
-parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+parser = argparse.ArgumentParser()
 parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='/dev/ttyUSB0')
-parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
-parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
-parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards', required=False, default=BoardIds.CYTON_DAISY_BOARD)
+parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards', required=False, default=BoardIds.CYTON_DAISY_BOARD) #Use BoardIds.SYNTHETIC_BOARD if you want to test with synthetic board
 parser.add_argument('--master-board', type=int, help='master board id for streaming and playback boards', required=False, default=BoardIds.NO_BOARD)
 args = parser.parse_args()
 
 params = BrainFlowInputParams()
-params.ip_port = args.ip_port
-params.ip_protocol = args.ip_protocol
-params.ip_address = args.ip_address
 params.serial_port = args.serial_port
-params.mac_address = args.mac_address
-params.serial_number = args.serial_number
 params.board_id = args.board_id
 params.master_board = args.master_board
 
@@ -46,18 +36,18 @@ with open('sessions/EEG-'+moment+'.csv', 'w+') as f:
 	writer.writerow(['Sample Index', 'EXG Channel 0', 'EXG Channel 1', 'EXG Channel 2', 'EXG Channel 3', 'EXG Channel 4', 'EXG Channel 5', 'EXG Channel 6', 'EXG Channel 7', 'EXG Channel 8', 'EXG Channel 9', 'EXG Channel 10', 'EXG Channel 11', 'EXG Channel 12', 'EXG Channel 13', 'EXG Channel 14', 'EXG Channel 15', 'Accel Channel 0', 'Accel Channel 1', 'Accel Channel 2', 'Other', 'Other', 'Other', 'Other', 'Other', 'Other', 'Other', 'Analog Channel 0', 'Analog Channel 1', 'Analog Channel 2', 'Timestamp', 'Other'])
 	f.close()
 
-board = BoardShim(args.board_id, params)    #Uses synth board as default for testing
-board.prepare_session()           #prepare streaming session, init resources, you need to call it before any other BoardShim object methods
+board = BoardShim(args.board_id, params)
+board.prepare_session()           #Prepare streaming session, init resources, you need to call it before any other BoardShim object methods
 board.start_stream ()       #Start streaming data, this method stores data in ring buffer
 
 while True:
 	try:               
         	time.sleep(4)
-        	data = board.get_board_data()  # get all data and remove it from internal buffer
+        	data = board.get_board_data()  #Get all data and remove it from internal buffer
         	DataFilter.write_file(data, 'sessions/EEG-'+moment+'.csv', 'a')
 		
 	except KeyboardInterrupt:
 		data = board.get_board_data()
 		DataFilter.write_file(data, 'sessions/EEG-'+moment+'.csv', 'a')
-		board.release_session()     #release all resources
+		board.release_session()     #Release all resources
 		break
